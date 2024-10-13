2ab0fae54e1b786abf28d38dc7a399b65edcf851
Witcher770 <maksimuryashov@gmail.com>
2024-10-14 01:27:51 +0300

    еще один новый проект, тк предыдущий как-то сломался, но файлики в основном без изменений

--- app/src/main/java/com/example/cookingai/MainActivity.kt
+++ app/src/main/java/com/example/cookingai/MainActivity.kt
@@ -5,19 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -38,16 +32,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Preview(showBackground = true)
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "MainScreen") {
         composable("MainScreen") { MainScreenshot(navController)}
-        composable("Settings") { SettiSreen(navController) }
+        composable("Settin") { SettiSreen(navController) }
         composable("CookingCamera") { CookingCamera(navController) }
         composable("History") { History(navController) }
     }
 }
-
-
--- app/src/main/java/com/example/cookingai/ui/theme/History.kt
+++ app/src/main/java/com/example/cookingai/ui/theme/History.kt
@@ -1,8 +1,15 @@
 package com.example.cookingai.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -10,12 +17,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+
 
 @Composable
 public fun History(navController: NavController) {
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize().background(Color.Red),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,) {
         Text(text = "В будущем тут будет история, но пока только кнопочка")
@@ -26,6 +37,25 @@ public fun History(navController: NavController) {
             colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF98d7ff))
         ) {
             Text(text = "Назад в меню")
+        }
+
+        Box(modifier = Modifier
+            .background(Color.Yellow)
+            .padding(20.dp)) {
+            LazyColumn(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.Green)
+            ) {
+                items(count = 10) {
+                    Text(
+                        text = "Resept $it",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(vertical = 30.dp)
+                    )
+                }
+            }
         }
     }
 }
